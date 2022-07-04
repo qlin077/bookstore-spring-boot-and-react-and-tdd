d749a317e39b81038c114c5c9f6aaab315c1497a
qlin077 <qingqing.lin@baruchcollege.edu>
2022-07-04 19:28:25 +0800

    first commit

---
+++ src/main/java/com/example/qlinjavaspringbookstoredemo/QlinJavaSpringBookstoreDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.qlinjavaspringbookstoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlinJavaSpringBookstoreDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QlinJavaSpringBookstoreDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/qlinjavaspringbookstoredemo/QlinJavaSpringBookstoreDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qlinjavaspringbookstoredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlinJavaSpringBookstoreDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
